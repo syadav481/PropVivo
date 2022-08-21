c95a09121a46693aa22fd5dcc6eb3605aba48670
swarajyadav <swarajy@umich.edu>
2022-08-21 16:15:53 -0700

    Initial commit.

---
+++ src/components/Navbar.js
@@ -0,0 +1,53 @@
+import "./Navbar.css";
+import pvLogo from "../assets/pvLogo.png";
+import pvAssistant from "../assets/pvAssistant.png";
+import SearchBar from "./SearchBar";
+import React, { useEffect, useRef, useState } from "react";
+import { createPortal } from "react-dom";
+
+const RenderInWindow = (props) => {
+  const [container, setContainer] = useState(null);
+  const newWindow = useRef(window);
+
+  useEffect(() => {
+    const div = document.createElement("div");
+    setContainer(div);
+  }, []);
+
+  useEffect(() => {
+    if (container) {
+      newWindow.current = window.open(
+        "https://pmapp.devpropvivo.com/DigitalAssistant",
+        "",
+        "width=600,height=400,left=200,top=200"
+      );
+      newWindow.current.document.body.appendChild(container);
+      const curWindow = newWindow.current;
+      return () => curWindow.close();
+    }
+  }, [container]);
+
+  return container && createPortal(props.children, container);
+};
+
+function Navbar() {
+  const [open, setOpen] = useState();
+  return (
+    <div className="Navbar">
+      <div className="left-side">
+        <a href="https://www.devpropvivo.com/" target="_blank">
+          <img src={pvLogo} width="50" height="50"></img>
+        </a>
+      </div>
+      <div className="right-side">
+        <SearchBar />
+        <button className="PvAsstBtn" onClick={() => setOpen(true)}>
+          <img src={pvAssistant} width="52" height="52"></img>
+        </button>
+        {open && <RenderInWindow>hello world</RenderInWindow>}
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
---
+++ src/components/SearchBar.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./SearchBar.css";
+function SearchBar() {
+  return (
+    <div className="SearchDiv">
+      <input className="SearchBar" type="text" placeholder="Search here..." />
+    </div>
+  );
+}
+
+export default SearchBar;
